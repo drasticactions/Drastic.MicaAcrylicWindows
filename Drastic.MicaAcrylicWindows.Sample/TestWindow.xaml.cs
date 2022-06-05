e7fb72936c8a988f84370d965aaf9004256815c0
Timothy Miller <innerlogic4321@gmail.com>
2022-06-04 23:18:25 -0400

    Fix nuget packaging, update sample

--- Drastic.MicaAcrylicWindows.Sample/TestWindow.xaml.cs
+++ Drastic.MicaAcrylicWindows.Sample/TestWindow.xaml.cs
@@ -1,13 +1,7 @@
 ﻿using Drastic.UI.Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Drastic.MicaAcrylicWindows.Sample
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class TestWindow : MicaWindow
     {
         public TestWindow()
